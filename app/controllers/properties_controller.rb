eba8de0c429c514d60e9f140e72e9159b6a61642
fujitamariko <fujyta.mrk@gmail.com>
2021-09-02 14:40:42 +0900

    コントローラーのリファクタリング

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,4 +5,13 @@ class PropertiesController < ApplicationController
     def new
         @property = Property.new
     end
+    def create
+        Property.create(property_params)
+        redirect_to new_property_path
+    end
+
+    private
+    def property_params
+        params.require(:property).permit(:name, :rent, :address, :age, :content)
+    end
 end
